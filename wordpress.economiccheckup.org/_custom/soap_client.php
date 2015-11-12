39cf74540a16cf2cdbd0959d118fb2984e91181c
Stephen Engels <sgengels@gmail.com>
2015-11-12 15:47:30 -0500

    link updated

--- wordpress.economiccheckup.org/_custom/soap_client.php
+++ wordpress.economiccheckup.org/_custom/soap_client.php
@@ -1,7 +1,7 @@
 <?PHP
 require_once('CachedSoapClient.php');
 
-$url = "https://redesign.benefitscheckup.org/util/remotescreening_3_7.cfc?WSDL";
+$url = "https://www.benefitscheckup.org/util/remotescreening_3_7.cfc?WSDL";
 //cache or no cache
 //$soapClient = new CachedSoapClient($url, array('cache_wsdl' => WSDL_CACHE_NONE));
 $soapClient = new CachedSoapClient($url, array('cache_wsdl' => WSDL_CACHE_BOTH), $subsetID, doScreening);
