871ff8202e0307c7f9e107081fc7a382feab8d78
Stephen Engels <sgengels@gmail.com>
2015-11-20 15:48:09 -0600

    updated qa and live params

--- wordpress.economiccheckup.org/_custom/soap_client.php
+++ wordpress.economiccheckup.org/_custom/soap_client.php
@@ -1,16 +1,17 @@
 <?PHP
 require_once('CachedSoapClient.php');
-if (QA = 0){
-$subdomain 'www';
+if (QA == 1){
+$subdomain = 'www';
 $esiprod = 1;
 }
 else
 {
-$subdomain 'redesign';
+$subdomain = 'redesign';
 $esiprod = 0;
 }
 
 $url = "https://".$subdomain.".benefitscheckup.org/util/remotescreening_3_7.cfc?WSDL";
+
 //cache or no cache
 //$soapClient = new CachedSoapClient($url, array('cache_wsdl' => WSDL_CACHE_NONE));
 $soapClient = new CachedSoapClient($url, array('cache_wsdl' => WSDL_CACHE_BOTH), $subsetID, doScreening);
