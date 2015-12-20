4420a58f635222cffb7b312a2ec1bf03b904eef9
Stephen Engels <sgengels@gmail.com>
2015-12-19 19:41:01 -0500

    quickcheck updated

--- wordpress.benefitscheckup.org/_custom/reportbody_bcu_quickcheck.php
+++ wordpress.benefitscheckup.org/_custom/reportbody_bcu_quickcheck.php
@@ -36,21 +36,80 @@ require_once('report/report_calculations.php');
             <form><a data-toggle="modal" role="button" class="btn btn-large" href="#" onClick="javascript:PrintDiv();"><i class="icon-print "></i> Print Your Report</a></form>
 </div>
 
-
- 
-    
+		
+   
 </div> <!--span8-->
 <div class="span4">
    	<div class="well sidebar-nav " style="z-index: 999; position: static; width:220px; top: 191px;">
         <legend><i class="icon-list-alt "></i> Sections</legend>
         <ol class="nav nav-list">
-	    <li id="health_li" class=""><a href="#health"><i class="icon-ambulance "></i> Medicaid/Health </a></li>
-            <li id="money_li"  class=""><a href="#money"><i class="icon-money "></i> Income Assistance </a></li>
-	    <li id="rx_li" class=""><a href="#rx"><i class="icon-medkit "></i> Prescription Assistance </a></li>
-            <li id="nutrition_li" class=""><a href="#nutrition"><i class="icon-heart "></i> Nutrition Assistance </a></li>
-            <li id="food_li" class=""><a href="#food"><i class="icon-coffee "></i> Food/Meal Supplements </a></li>
-            <li id="tax_li" class=""><a href="#tax"><i class="icon-building "></i> Property Tax Relief </a></li>
-            <li id="veteran_li" class=""><a href="#veteran"><i class="icon-user "></i> Veteran Assistance</a></li>
+	<?PHP
+ foreach ($cats as $c => $row)
+					{
+
+						$cat_title = $row["CATEGORY_TITLE"];
+						$sort_id = $row["SORT"];
+						$cat_code = $row["CODE"];
+						$programcategory_id = $row["PROGRAMCATEGORY_ID"];
+						//echo $cat_code;
+						if ($cat_code == 'veteran'){
+						$sectioncode = 'bcu_quickcheck_report_heading_veteran';
+						$iconcode = 'user';
+						$interested = 'bcuqc_category_veteran';
+						}
+						elseif ($cat_code == 'taxrelief'){
+						$sectioncode = 'bcu_quickcheck_report_heading_taxrelief';
+						$iconcode = 'building';
+						$interested = 'bcuqc_category_property_taxrelief';
+						}
+						
+						elseif ($cat_code == 'foodsupp'){
+						$sectioncode = 'bcu_quickcheck_report_heading_foodsupp';
+						$iconcode = 'coffee';
+						$interested = 'bcuqc_category_foodsupp';
+						}
+
+						elseif ($cat_code == 'nutrition'){
+						$sectioncode = 'bcu_quickcheck_report_heading_nutrition';
+						$iconcode = 'heart';
+						$interested = 'bcuqc_category_nutrition';
+						}
+						elseif ($cat_code == 'rxgov'){
+						$sectioncode = 'bcu_quickcheck_report_heading_rx';
+						$iconcode = 'medkit';
+						$interested = 'bcuqc_category_rx';
+						}
+						elseif ($cat_code == 'medicaid'){
+						$sectioncode = 'bcu_quickcheck_report_heading_medicaid';
+						$iconcode = 'ambulance';
+						$interested = 'bcuqc_category_medicaid';
+						}
+						elseif ($cat_code == 'income'){
+						$sectioncode = 'bcu_quickcheck_report_heading_income';
+						$iconcode = 'money';
+						$interested = 'bcuqc_category_income';
+						}
+													
+						//echo $cat_code.$programcategory_id;
+						//echo $sort_id;
+						$thischecked = false;
+						foreach ($fields as $f => $row)
+						{
+						if ($row["ANSWERFIELD"] == $interested) {
+						$zip = $row["RESPONSE"];
+						//echo "field:".$interested;
+						$thischecked = true;			
+						}
+						
+
+						}
+
+						if ($thischecked == true) {
+						echo '<li id="leftnav_'.$interested.'" class=""><a href="#'.$interested.'"><i class="icon-'.$iconcode.' "></i> '.$cat_title.' </a></li>';
+						}
+					}	
+?>
+	    
         </ol><BR>
 
         <legend><i class="icon-file-alt "></i> Resources</legend>
