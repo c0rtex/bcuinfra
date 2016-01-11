07d70d60ef06a8ef53abbdb9686c7d7d5a6d582b
Lynna Cekova <lynna.cekova@gmail.com>
2016-01-11 16:39:16 -0500

    merging bcuVisualChanges into dev

--- wordpress.benefitscheckup.org/wp-content/themes/bcuwptheme/bcu.js
+++ wordpress.benefitscheckup.org/wp-content/themes/bcuwptheme/bcu.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-jQuery(document).ready(function () {
-    var $zip_code = jQuery('#bcuQuickcheckForm input#zip');
-    var validated = 0;
-=======
 jQuery(document).ready(function() {
   var $zip_code = jQuery('#bcuQuickcheckForm input#zip');
   var validated = 0;
@@ -41,233 +36,170 @@ jQuery(document).ready(function() {
       jQuery('#esiQuickcheckCheckboxes').before('<div id="invalidInterest" class="alert alert-danger alert-bcuQuickCheck"><i class="icon-warning-sign"></i> <strong>You must select at least one area of interest.</strong></div>');
       validated = 0;
     }
->>>>>>> bcuVisualChanges
 
-    $(window).resize(function () {
-        responsiveQcTable();
-    });
+    if (validated == 0) {
+      // Do not submission if the form fails validation
+      return false;
+    }
+    else {
+      jQuery('form#bcuQuickcheckForm').submit();
+    }
 
-    jQuery('#esiQuickcheckResultsButton').on('click', function (e) {
-        e.preventDefault();
+  });
 
-        validated = 1;
-        // Validate specific input parameters
-        removeZipAlerts();
-        jQuery('#invalidInterest').remove();
-        if ($zip_code.val().length != 5) {
-            $zip_code.before('<div id="invalidZipNumbers" class="alert alert-danger alert-bcuQuickCheck"><i class="icon-warning-sign"></i> <strong>You must enter a valid ZIP code containing all 5 numbers. Please try again.</strong></div>');
-            validated = 0;
+  // Properly validate the entered ZIP code and link it to a state
+  $zip_code.keyup(function() {
+    console.log("keyup5");
+    console.log ($zip_code.val().length);
+    if (jQuery(this).val().length == 5) {
+      console.log("5");
+      var isValidZip = /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(jQuery(this).val());
+      if(!isValidZip){
+        //alert('You must enter a valid ZIP code containing all numbers. Please try again');
+        if (jQuery('#invalidZipLetters').length == 0){
+          removeZipAlerts();
+          $zip_code.before('<div id="invalidZipLetters" class="alert alert-danger alert-bcuQuickCheck"><i class="icon-warning-sign"></i> <strong>You must enter a valid ZIP code containing all numbers. Please try again.</strong></div>');
         }
-        if (jQuery('#zyxzip_zip_abbrev').val() == '??') {
-            $zip_code.before('<div id="invalidZip" class="alert alert-danger alert-bcuQuickCheck"><i class="icon-warning-sign"></i> This is an invalid ZIP code. Please try again.</div>');
-            validated = 0;
-        }
-        if (!(jQuery('#esiQuickcheckCheckboxes :checkbox:checked').length > 0)) {
-            jQuery('#esiQuickcheckCheckboxes').before('<div id="invalidInterest" class="alert alert-danger alert-bcuQuickCheck"><i class="icon-warning-sign"></i> <strong>You must select at least one area of interest.</strong></div>');
-            validated = 0;
-        }
+        jQuery(this).val('');
+        return false;
+      };
 
-        if (validated == 0) {
-            // Do not submission if the form fails validation
-            return false;
-        }
-        else {
-            jQuery('form#bcuQuickcheckForm').submit();
-        }
-
-    });
-
-    // Properly validate the entered ZIP code and link it to a state
-    $zip_code.keyup(function () {
-        console.log("keyup5");
-        console.log($zip_code.val().length);
-        if (jQuery(this).val().length == 5) {
-            console.log("5");
-            var isValidZip = /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(jQuery(this).val());
-            if (!isValidZip) {
-                //alert('You must enter a valid ZIP code containing all numbers. Please try again');
-                if (jQuery('#invalidZipLetters').length == 0) {
-                    removeZipAlerts();
-                    $zip_code.before('<div id="invalidZipLetters" class="alert alert-danger alert-bcuQuickCheck"><i class="icon-warning-sign"></i> <strong>You must enter a valid ZIP code containing all numbers. Please try again.</strong></div>');
-                }
-                jQuery(this).val('');
-                return false;
-            }
-            ;
-
-            var threeDigits = jQuery(this).val().substring(0, 3);
+      var threeDigits = jQuery(this).val().substring (0, 3);
 //console.log ("three gidig" + threeDigits);
-            if (threeDigits == "006" || threeDigits == "007" || threeDigits == "008" || threeDigits == "009" || threeDigits == "969") {
+      if (threeDigits == "006" || threeDigits == "007" || threeDigits == "008" || threeDigits == "009" || threeDigits == "969"){
 
-                if (jQuery('#invalidZip').length == 0) {
+        if (jQuery('#invalidZip').length == 0){
 
-                    removeZipAlerts();
-                    $zip_code.before('<div id="invalidZip" class="alert alert-danger alert-bcuQuickCheck"><i class="icon-warning-sign"></i> The zip code you entered is invalid. Please enter a zip code for the U.S. or District of Columbia.</div>');
-                }
-                jQuery(this).val('');
-                return false;
-            }
-
-            var myVal = jQuery(this).val();
-            jQuery.ajax({
-                url: 'https://www.benefitscheckup.org/cf/com/bcu/ZipFunctions.cfc?method=getStateFromZip',
-                datatype: 'json',
-                data: 'zipcode=' + myVal,
-                cache: false,
-                success: function (StateId) {
-                    var newcode = eval('(' + StateId + ')');
-//console.log("invliad zip length "+jQuery('#invalidZip').length);
-                    if (!newcode || StateId == 'false') {
-                        stcode = '';
-                        stname = '';
-                        if (jQuery('#invalidZip').length == 0) {
-
-                            removeZipAlerts();
-                            $zip_code.before('<div id="invalidZip" class="alert alert-danger alert-bcuQuickCheck"><i class="icon-warning-sign"></i> This is an invalid ZIP code. Please try again.</div>');
-                        }
-                        jQuery('#zyxzip_zip_abbrev').val('??');
-                        jQuery('#zyxzip_zip_abbrev').css('color', 'black');
-                        jQuery('#zyxzip_zip_abbrev').css('background', '#ffcccc');
-                        return false;
-                    } else {
-
-                        removeZipAlerts();
-
-                        stcode = newcode.DATA[0][0];
-                        stname = newcode.DATA[0][1];
-                        jQuery('#zyxzip_zip_abbrev').val(stcode);
-                        jQuery('#zyxzip_zip_abbrev').css('color', 'black');
-                        if (stcode == 'NY') {
-                            jQuery('#zyxzip_zip_abbrev').css('background', '#ccffcc');
-                        } else {
-                            jQuery('#zyxzip_zip_abbrev').css('background', '#ccccff');
-                        }
-                    }
-                    ;
-                }
-            });
-        } else {
-            stcode = '';
-            jQuery('#zyxzip_zip_abbrev').val('n/a');
-            jQuery('#zyxzip_zip_abbrev').css('color', '#666666');
-            jQuery('#zyxzip_zip_abbrev').css('background', '#dddddd');
+          removeZipAlerts();
+          $zip_code.before('<div id="invalidZip" class="alert alert-danger alert-bcuQuickCheck"><i class="icon-warning-sign"></i> The zip code you entered is invalid. Please enter a zip code for the U.S. or District of Columbia.</div>');
         }
-    });
+        jQuery(this).val('');
+        return false;
+      }
 
-    $zip_code.blur(function () {
-        var isValidZip = /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(jQuery(this).val());
-        if (!isValidZip) {
-            //alert('You must enter a valid ZIP code containing all 5 numbers. Please try again');
+      var myVal = jQuery(this).val();
+      jQuery.ajax({
+        url: 'https://www.benefitscheckup.org/cf/com/bcu/ZipFunctions.cfc?method=getStateFromZip',
+        datatype:'json',
+        data: 'zipcode='+myVal,
+        cache:false,
+        success: function(StateId){
+          var newcode = eval('('+StateId+')');
+//console.log("invliad zip length "+jQuery('#invalidZip').length);
+          if(!newcode || StateId == 'false'){
+            stcode = '';
+            stname = '';
+            if (jQuery('#invalidZip').length == 0){
+
+              removeZipAlerts();
+              $zip_code.before('<div id="invalidZip" class="alert alert-danger alert-bcuQuickCheck"><i class="icon-warning-sign"></i> This is an invalid ZIP code. Please try again.</div>');
+            }
+            jQuery('#zyxzip_zip_abbrev').val('??');
+            jQuery('#zyxzip_zip_abbrev').css('color','black');
+            jQuery('#zyxzip_zip_abbrev').css('background','#ffcccc');
+            return false;
+          } else {
 
             removeZipAlerts();
-            $zip_code.before('<div id="invalidZipNumbers" class="alert alert-danger alert-bcuQuickCheck"><i class="icon-warning-sign"></i> <strong>You must enter a valid ZIP code containing all 5 numbers. Please try again.</strong></div>');
-            jQuery(this).val('');
-            return false;
-        }
-        ;
 
-        jQuery.ajax({
-            url: 'https://www.benefitscheckup.org/cf/com/bcu/ZipFunctions.cfc?method=isZipInState',
-            datatype: 'json',
-            data: 'zipcode=' + jQuery(this).val() + '&state_id=NY',
-            cache: false,
-            success: function (StateId) {
-                var newcode = eval('(' + StateId + ')');
-                if (!newcode && stname != '') {
-                    if (1 == 1) {
-                        if ($zip_code.val().length == 5) {
-                            jQuery('#zyxzip_zip_abbrev').val(stcode);
-                            jQuery('#zyxzip_zip_abbrev').css('color', 'black');
-
-                            jQuery('#zyxzip_zip_abbrev').css('background', '#ccccff');
-
-                        } else {
-                            stcode = '';
-                            jQuery('#zyxzip_zip_abbrev').val('n/a');
-                            jQuery('#zyxzip_zip_abbrev').css('color', '#666666');
-                            jQuery('#zyxzip_zip_abbrev').css('background', '#dddddd');
-                        }
-                    }
-                    ;
-                }
-                ;
-            },
-            error: function () {
-                console.log("Ajax error for the zip validation.");
+            stcode = newcode.DATA[0][0];
+            stname = newcode.DATA[0][1];
+            jQuery('#zyxzip_zip_abbrev').val(stcode);
+            jQuery('#zyxzip_zip_abbrev').css('color','black');
+            if (stcode == 'NY'){
+              jQuery('#zyxzip_zip_abbrev').css('background','#ccffcc');
+            } else {
+              jQuery('#zyxzip_zip_abbrev').css('background','#ccccff');
             }
-        });
-    });
-
-
-    function removeZipAlerts() {
-        if (jQuery('#invalidZip').length != 0) {
-            jQuery('#invalidZip').remove();
+          };
         }
-        if (jQuery('#invalidZipNumbers').length != 0) {
-            jQuery('#invalidZipNumbers').remove();
-        }
-        if (jQuery('#invalidZipLetters').length != 0) {
-            jQuery('#invalidZipLetters').remove();
-        }
+      });
+    } else {
+      stcode = '';
+      jQuery('#zyxzip_zip_abbrev').val('n/a');
+      jQuery('#zyxzip_zip_abbrev').css('color','#666666');
+      jQuery('#zyxzip_zip_abbrev').css('background','#dddddd');
     }
+  });
 
-    jQuery('#esiQuickcheckCheckboxes').prepend('<div id="second-column" class="last-column"></div>');
-    jQuery('#esiQuickcheckCheckboxes').prepend('<div id="first-column" class="first-column"></div>');
-    jQuery('#esiQuickcheckCheckboxes > div.checkbox:lt(4)').appendTo('.first-column');
-    jQuery('#esiQuickcheckCheckboxes > div.checkbox:lt(3)').appendTo('.last-column');
-    jQuery('#tr_bcuqc_category_veteran').detach().appendTo('.last-column');
+  $zip_code.blur(function() {
+    var isValidZip = /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(jQuery(this).val());
+    if(!isValidZip){
+      //alert('You must enter a valid ZIP code containing all 5 numbers. Please try again');
 
-    responsiveQcTable();
+      removeZipAlerts();
+      $zip_code.before('<div id="invalidZipNumbers" class="alert alert-danger alert-bcuQuickCheck"><i class="icon-warning-sign"></i> <strong>You must enter a valid ZIP code containing all 5 numbers. Please try again.</strong></div>');
+      jQuery(this).val('');
+      return false;
+    };
+
+    jQuery.ajax({
+      url: 'https://www.benefitscheckup.org/cf/com/bcu/ZipFunctions.cfc?method=isZipInState',
+      datatype:'json',
+      data: 'zipcode='+jQuery(this).val()+'&state_id=NY',
+      cache:false,
+      success: function(StateId){
+        var newcode = eval('('+StateId+')');
+        if(!newcode && stname != ''){
+          if (1 ==1) {
+            if ($zip_code.val().length == 5) {
+              jQuery('#zyxzip_zip_abbrev').val(stcode);
+              jQuery('#zyxzip_zip_abbrev').css('color','black');
+
+              jQuery('#zyxzip_zip_abbrev').css('background','#ccccff');
+
+            } else {
+              stcode = '';
+              jQuery('#zyxzip_zip_abbrev').val('n/a');
+              jQuery('#zyxzip_zip_abbrev').css('color','#666666');
+              jQuery('#zyxzip_zip_abbrev').css('background','#dddddd');
+            }
+          };
+        };
+      },
+      error: function(){
+        console.log ("Ajax error for the zip validation.");
+      }
+    });
+  });
+
+
+  function removeZipAlerts(){
+    if (jQuery('#invalidZip').length != 0){
+      jQuery('#invalidZip').remove();
+    }
+    if (jQuery('#invalidZipNumbers').length != 0){
+      jQuery('#invalidZipNumbers').remove();
+    }
+    if (jQuery('#invalidZipLetters').length != 0){
+      jQuery('#invalidZipLetters').remove();
+    }
+  }
+
+  jQuery('#esiQuickcheckCheckboxes').prepend('<div class="last-column"></div>');
+  jQuery('#esiQuickcheckCheckboxes').prepend('<div class="first-column"></div>');
+  jQuery('#esiQuickcheckCheckboxes > div.checkbox:lt(4)').appendTo('.first-column');
+  jQuery('#esiQuickcheckCheckboxes > div.checkbox:lt(3)').appendTo('.last-column');
+  jQuery('#tr_bcuqc_category_veteran').detach().appendTo('.last-column');
 
 //  Check if overlay overlay has been set to true
-    function getURLParameter(name) {
-        return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null
-    }
+  function getURLParameter(name) {
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
+  }
 
-    //if the overlay URL parameter is set to true then show the quick check modal.
-    if (getURLParameter("overlay") == "true") {
-        jQuery('#bcu_quickcheck').modal('show')
-    }
-
-    //clears all alerts
-    function clearErrorsOnHide() {
-        jQuery(".alert").remove();
-    }
-
-    //clear all alerts when the modal dialog is closed.
-    jQuery(".modal").on("hidden.bs.modal", clearErrorsOnHide);
-
-<<<<<<< HEAD
-});
-=======
   //Gets the url parameters
   if(getURLParameter("overlay") == "true"){
     jQuery('#bcu_quickcheck').modal('show')
   }
->>>>>>> bcuVisualChanges
 
-/**
- * Remove the second column from the table and appends the to the
- * end of the first column. Should resolve small screen text overflow issue.
- */
-function responsiveQcTable() {
+  //clears all alerts
+  function clearErrorsOnHide() {
+    jQuery(".alert").remove();
+  }
 
-    var food = "#tr_bcuqc_category_foodsupp";
-    var utility = "#tr_bcuqc_category_utility";
-    var taxes = "#tr_bcuqc_category_property_taxrelief";
-    var veteran = "#tr_bcuqc_category_veteran";
+  //clear all alerts when the modal dialog is closed.
+  jQuery(".modal").on("hidden.bs.modal", clearErrorsOnHide);
 
-<<<<<<< HEAD
-    if ($(window).width() <= 480) {
-        $("#second-column").children().appendTo("#first-column")
-    }
-    else {
-        $(food).appendTo("#second-column");
-        $(utility).appendTo("#second-column");
-        $(taxes).appendTo("#second-column");
-        $(veteran).appendTo("#second-column")
-    }
-=======
 });
 
 function responsiveMenu() {
@@ -295,5 +227,4 @@ function responsiveMenu() {
     $("#getStartedESIButtonImg").css("margin-top", "0px");
   }
 
->>>>>>> bcuVisualChanges
 }
