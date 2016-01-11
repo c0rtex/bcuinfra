0b0e39feedbeb0472e1623b2c0bc0aebc831cc29
Lynna Cekova <lynna.cekova@gmail.com>
2016-01-11 17:52:02 -0500

    responsiveness fix for the columns in overlay.

--- wordpress.benefitscheckup.org/wp-content/themes/bcuwptheme/bcu.js
+++ wordpress.benefitscheckup.org/wp-content/themes/bcuwptheme/bcu.js
@@ -13,6 +13,7 @@ jQuery(document).ready(function () {
 
     $(window).resize(function () {
         responsiveMenu();
+        responsiveQcTable();
     });
 
     $("#esiMenuToggleButton").click(function () {
