774a9a3ee5b080bc36fd6c088d17a7e759b8c196
raukasov <microvax2>
2016-01-13 22:13:51 +0300

    Added new web services for question sets

--- branch.benefitscheckup.org/code/js/ques/services.js
+++ branch.benefitscheckup.org/code/js/ques/services.js
@@ -8,7 +8,7 @@ services.factory('questionSet1', ['$resource',
   function($resource){
     return $resource('http://localhost/cf/components/questionSet1.cfc?method=getQuestionSet&subset_id=:subset_id&state_id=:state_id&partner_id=:partner_id&callback=JSON_CALLBACK', {}, {
       query: {method:'JSONP',
-              params:{subset_id:0,state_id:'',partner_id:0},
+              params:{subset_id:0,state_id:'',partner_id:0,org_id:0},
               isArray:true}
     });
   }]);
@@ -17,7 +17,25 @@ services.factory('questionSet2', ['$resource',
     function($resource){
         return $resource('http://localhost/cf/components/questionSet2.cfc?method=getQuestionSet&callback=JSON_CALLBACK', {}, {
             query: {method:'JSONP',
-                params:{subset_id:0,state_id:'',partner_id:0},
+                params:{subset_id:0,state_id:'',partner_id:0,org_id:0},
+                isArray:true}
+        });
+    }]);
+
+services.factory('questionSet3', ['$resource',
+    function($resource){
+        return $resource('http://localhost/cf/components/questionSet3.cfc?method=getQuestionSet&callback=JSON_CALLBACK', {}, {
+            query: {method:'JSONP',
+                params:{subset_id:0,state_id:'',partner_id:0,org_id:0},
+                isArray:true}
+        });
+    }]);
+
+services.factory('questionSet4', ['$resource',
+    function($resource){
+        return $resource('http://localhost/cf/components/questionSet4.cfc?method=getQuestionSet&callback=JSON_CALLBACK', {}, {
+            query: {method:'JSONP',
+                params:{subset_id:0,state_id:'',partner_id:0,org_id:0},
                 isArray:true}
         });
     }]);
