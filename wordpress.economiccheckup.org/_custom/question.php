06a655e995a4f4dba049417f75ea44acb5d84fcc
Lynna Cekova <lynna.cekova@gmail.com>
2016-01-27 16:12:39 -0500

    Test title grab.

--- wordpress.economiccheckup.org/_custom/question.php
+++ wordpress.economiccheckup.org/_custom/question.php
@@ -1,16 +1,16 @@
-<?php 
+<?php
 
 //Get question display text.
 $query_getDisplayTextByID = "
 						select dl.display_text
 						from display_language dl, language l
-						where dl.display_id=".$questionDisplayID."
+						where dl.display_id=" . $questionDisplayID . "
 						and dl.language_id in ('EN')
 						and dl.language_id=l.language_id
 						and l.active = 1
 						order by l.default_flag
 						";
-$questionDisplay_query = $bcudb->get_row($query_getDisplayTextByID); 
+$questionDisplay_query = $bcudb->get_row($query_getDisplayTextByID);
 $questionText = $questionDisplay_query->display_text;
 //$questionString = $questionText;
 //$findA   = '[[';
@@ -21,29 +21,28 @@ $questionText = $questionDisplay_query->display_text;
 $line = $questionText;
 
 $line = preg_replace_callback(
-        '/\[\[[^\]]*\]\]/' // / is a delimiter denoting start and end of expression
-,
-        function ($matches) {
+    '/\[\[[^\]]*\]\]/' // / is a delimiter denoting start and end of expression
+    ,
+    function ($matches) {
 
-$parts = explode('|', $matches[0]);
-if ( sizeOf($parts) == 2){
- $cleanedLinkTitle = substr ($parts[1], 0, -2);
- $theKey = $cleanedLinkTitle;
- //echo '<h3>size 2:'.$theKey .'</h3>';
-}
-else if (sizeOf($parts) == 3){
- $cleanedLinkTitle = substr ($parts[2], 0, -2);
- $theKey = $parts[1];
-}
-$defContent = $theKey;
-$defContent = getHelpDisplayTextbyCode($theKey);
+        $parts = explode('|', $matches[0]);
+        if (sizeOf($parts) == 2) {
+            $cleanedLinkTitle = substr($parts[1], 0, -2);
+            $theKey = $cleanedLinkTitle;
+            //echo '<h3>size 2:'.$theKey .'</h3>';
+        } else if (sizeOf($parts) == 3) {
+            $cleanedLinkTitle = substr($parts[2], 0, -2);
+            $theKey = $parts[1];
+        }
+        $defContent = $theKey;
+        $defContent = getHelpDisplayTextbyCode($theKey);
 
- $printLink =  '<a title="'.$cleanedLinkTitle.'" data-content="'.$defContent.'" data-placement="top" data-toggle="popover" href="#" data-original-title="'.$cleanedLinkTitle.'">'.$cleanedLinkTitle.'</a>';
- return $printLink;
+        $printLink = '<a title="' . $cleanedLinkTitle . '" data-content="' . $defContent . '" data-placement="top" data-toggle="popover" href="#" data-original-title="' . $cleanedLinkTitle . '">' . $cleanedLinkTitle . '</a>';
+        return $printLink;
 
-        },
-        $questionText
-    );
+    },
+    $questionText
+);
 
 $questionText = $line;
 
@@ -66,43 +65,39 @@ INNER JOIN question_help ON `help`.help_id = question_help.help_id
 INNER JOIN display_language as dl ON `help`.display_id = dl.display_id
 INNER JOIN display_language as dl2 ON `help`.title_display_id = dl2.display_id
 WHERE
-question_help.question_id = '".$questionID."'
+question_help.question_id = '" . $questionID . "'
 and dl2.language_id = 'EN'
 and dl.language_id = 'EN'
 ";
 $getQuestionHelp_query = $bcudb->get_results($query_getQuestionHelp);
 //var_dump($getQuestionHelp_query);
-echo'
+echo '
 						<div class="clearfix">
-                        	<div class="row-fluid" id="row_'.$questionCode.'">
-                            	<div class="span1"><strong>'.$questionNumberTotal.'.</strong></div>
+                        	<div class="row-fluid" id="row_' . $questionCode . '">
+                            	<div class="span1"><strong>' . $questionNumberTotal . '.</strong></div>
                                 <div class="span11">					      	
-                                	<label for="normalSelect">'.$questionText;
+                                	<label for="normalSelect">' . $questionText;
 
 foreach ($getQuestionHelp_query as $questionHelpObj):
 
-$displayID = $questionHelpObj->display_id;
-$titleDisplayID = $questionHelpObj->title_display_id;
+    $displayID = $questionHelpObj->display_id;
+    $titleDisplayID = $questionHelpObj->title_display_id;
 
-$helpText = htmlentities(getDisplayTextbyID($displayID));
-$helpTitle = getDisplayTextbyID($titleDisplayID);
+    $helpText = htmlentities(getDisplayTextbyID($displayID));
+    $helpTitle = getDisplayTextbyID($titleDisplayID, $soapClient);
+
 //$questionHelpText = $questionHelpObj->$helptext;	
 //$questionHelpTitle = $questionHelpObj->$title;	
 //echo 	$questionHelpText;
 
-echo "Before title";
-echo $helpTitle;
-echo "After title";
+    echo '<a data-content="' . $helpText . '" title="' . $helpTitle . '" data-html="true"  data-toggle="popover" href="#" data-original-title="Help"><i class="icon-question-sign icon-large fonta1"></i></a>
+';
+endforeach;
+echo '									</label>';
+require('_custom/questiontype.php');
 
-echo'									
-									<a data-content="'.$helpText.'" title="'.$helpTitle.'" data-html="true"  data-toggle="popover" href="#" data-original-title="Help"><i class="icon-question-sign icon-large fonta1"></i></a>
-';	
-endforeach;							
-echo'									</label>';
-									require('_custom/questiontype.php');
-									
-                                    
-echo'                                </div>
+
+echo '                                </div>
                           	</div>
 						</div>
 ';
