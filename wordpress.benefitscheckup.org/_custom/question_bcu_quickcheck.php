c3f84a717949ece07860e1d8e4b5ed59d9a72b08
Lynna Cekova <lynna.cekova@gmail.com>
2016-01-27 11:58:03 -0500

    id change

--- wordpress.benefitscheckup.org/_custom/question_bcu_quickcheck.php
+++ wordpress.benefitscheckup.org/_custom/question_bcu_quickcheck.php
@@ -4,7 +4,7 @@ echo'
 						<div class="clearfix bcuQuestionSet">
                         	<div class="row-fluid" id="question_'.$questionCode.'">
                                 <span class="span12">
-                                	<label for="normalSelect" id="bcu_income_questions">'.$questionText;
+                                	<label for="normalSelect" id="options_'.$questionCode.'">'.$questionText;
 require('_custom/questiontype_bcu_quickcheck.php');
 echo '
 </span>
