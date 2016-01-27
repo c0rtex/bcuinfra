4f9e99ab13889be02a27c0052afebe21fc454455
Lynna Cekova <lynna.cekova@gmail.com>
2016-01-27 15:20:43 -0500

    Finding the tag.

--- wordpress.economiccheckup.org/_custom/question.php
+++ wordpress.economiccheckup.org/_custom/question.php
@@ -90,7 +90,7 @@ $helpTitle = getDisplayTextbyID($titleDisplayID);
 //$questionHelpTitle = $questionHelpObj->$title;	
 //echo 	$questionHelpText;						
 echo'									
-									<a data-content="'.$helpText.'" title="'.$helpTitle.'" data-html="true"  data-toggle="popover" href="#" data-original-title="Help"><i class="icon-question-sign icon-large"></i></a>
+									<a data-content="'.$helpText.'" title="'.$helpTitle.'" data-html="true"  data-toggle="popover" href="#" data-original-title="Help"><i class="icon-question-sign icon-large fonta"></i></a>
 ';	
 endforeach;							
 echo'									</label>';
