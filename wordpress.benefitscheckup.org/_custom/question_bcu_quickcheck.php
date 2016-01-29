fbe469cf47ecec05b34337b99bfc317314434cbf
Lynna Cekova <lynna.cekova@gmail.com>
2016-01-29 15:21:41 -0500

    grouping divs by question type now

--- wordpress.benefitscheckup.org/_custom/question_bcu_quickcheck.php
+++ wordpress.benefitscheckup.org/_custom/question_bcu_quickcheck.php
@@ -1,7 +1,7 @@
 <?php
 //Get question display text.
 echo'
-						<div class="clearfix bcuQuestionSet">
+						<div class="clearfix bcuQuestionSet id="'.$questionCode.'_group" ">
                         	<div class="row-fluid" id="question_'.$questionCode.'">
                                 <span class="span12">
                                 	<label for="normalSelect" id="options_'.$questionCode.'">'.$questionText;
