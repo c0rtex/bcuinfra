c37e694f81d1d9dc024a59fcf1b62bdcd44bb36a
Lynna Cekova <lynna.cekova@gmail.com>
2016-02-01 22:06:27 -0500

    titles cleaned.

--- wordpress.economiccheckup.org/_custom/question.php
+++ wordpress.economiccheckup.org/_custom/question.php
@@ -75,7 +75,7 @@ $line = preg_replace_callback(
 
         $question_title = getHelpTitleByCode($theKey);
 
-        $printLink = '<a title="' . $question_title . '" data-content="' . $defContent . '" data-placement="top" data-toggle="popover" href="#" data-original-title="' . $question_title . '">' . $cleanedLinkTitle . '</a>';
+        $printLink = '<a title="' . $question_title . '" data-content="' . $defContent . '" data-placement="top" data-toggle="popover" href="#" data-original-title="' . ucwords($cleanedLinkTitle) . '">' . strtolower($cleanedLinkTitle) . '</a>';
         return $printLink;
 
     },
