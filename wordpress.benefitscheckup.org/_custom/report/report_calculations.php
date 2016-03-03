07fc5f91f624e63da4d86c9562ad19f7155c79e0
Stephen Engels <sgengels@gmail.com>
2016-03-03 11:40:04 -0600

    10016 update

--- wordpress.benefitscheckup.org/_custom/report/report_calculations.php
+++ wordpress.benefitscheckup.org/_custom/report/report_calculations.php
@@ -31,9 +31,11 @@ $fields = $soapClient->getAFShadow($screeningID );
 						$county = $row["RESPONSE"];
 						//echo "county".$county;			
 						}
-						if ($row["ANSWERFIELD"] == 'bcuqc_income_3000') {
-						$income_exclusion = y;
-						//echo "Income Exclusion:".$income_exclusion;			
+						if ($row["ANSWERFIELD"] == 'bcuqc_income_list') {
+							if ($row["RESPONSE"] == 'bcuqc_income_3000') {
+							$income_exclusion = y;
+							}
+						echo "Income Exclusion:".$income_exclusion;			
 						}
 
 					}	
