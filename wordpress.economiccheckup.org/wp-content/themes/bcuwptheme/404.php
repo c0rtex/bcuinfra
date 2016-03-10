a6020989d3f9a0a02c7ad4172eb42a8bcf0930ea
Stephen Engels <sgengels@gmail.com>
2016-03-10 13:58:28 -0500

    404 template edits for ESI not found pages

--- wordpress.economiccheckup.org/wp-content/themes/bcuwptheme/404.php
+++ wordpress.economiccheckup.org/wp-content/themes/bcuwptheme/404.php
@@ -1,6 +1,4 @@
-
 <!doctype html>
-
 <!--[if lt IE 7]><html lang="en-US" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if (IE 7)&!(IEMobile)]><html lang="en-US" class="no-js lt-ie9 lt-ie8"><![endif]-->
 <!--[if (IE 8)&!(IEMobile)]><html lang="en-US" class="no-js lt-ie9"><![endif]-->
