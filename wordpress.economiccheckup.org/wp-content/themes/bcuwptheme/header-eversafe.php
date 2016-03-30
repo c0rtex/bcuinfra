f4dc751021434234ccbc724ea45876ce1c813ce5
Lynna Cekova <lynna.cekova@gmail.com>
2016-03-30 16:12:43 -0400

    Initial eversafe page template, header, and footer

---
+++ wordpress.economiccheckup.org/wp-content/themes/bcuwptheme/header-eversafe.php
@@ -0,0 +1,163 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<head>
+	<title>EverSafe</title>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="language" content="en-US" />
+	<meta name="lang" content="en" />
+	<meta name="description" content="EverSafe provides seniors and trusted caretakers with improved financial security and peace of mind. EverSafe uses technology to protect against financial abuse." />
+
+	<!-- Segment -->
+	<script type="text/javascript">
+		!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.1.0";
+			analytics.load("vHLeBeMj38GXZ9eY10k9uHnZE9SFPDP2");
+			analytics.page()
+		}}();
+	</script>
+	
+	<!-- Hotjar Tracking Code for www.eversafe.com -->
+	<script>
+		(function(h,o,t,j,a,r){
+			  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+			  h._hjSettings={hjid:105814,hjsv:5};
+			  a=o.getElementsByTagName('head')[0];
+			  r=o.createElement('script');r.async=1;
+			  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+			  a.appendChild(r);
+		})(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+	</script>
+	
+	<!-- AutoPilot -->
+	<script type="text/javascript">(function(){var c=encodeURIComponent,s="SCRIPT",d=document,e=d.createElement(s),t=d.getElementsByTagName(s)[0],p="t="+c(d.title||"")+"&u="+c(d.location.href||"")+"&r="+c(d.referrer||"");e.src="https://api.autopilothq.com/anywhere/0e50b065fd8846d9ae5023709474771ca87c1169c2f94599bf543cd2f168fcb2?"+p;e.type="text/javascript";e.async=true;t.parentNode.insertBefore(e, t);})();(function(){var c=encodeURIComponent,s="SCRIPT",d=document,e=d.createElement(s),t=d.getElementsByTagName(s)[0];e.src="https://api.autopilothq.com/anywhere/aa/0e50b065fd8846d9ae5023709474771ca87c1169c2f94599bf543cd2f168fcb2";e.type="text/javascript";e.async=false;t.parentNode.insertBefore(e, t);window.AutopilotAnywhere={_runQueue:[],run:function(){this._runQueue.push(arguments);}};if(!window.Autopilot) window.Autopilot=window.AutopilotAnywhere;})();</script>
+	
+	<!-- FaceBook -->
+	<script>(function() {
+		var _fbq = window._fbq || (window._fbq = []);
+		if (!_fbq.loaded) {
+			var fbds = document.createElement('script');
+			fbds.async = true;
+			fbds.src = '//connect.facebook.net/en_US/fbds.js';
+			var s = document.getElementsByTagName('script')[0];
+			s.parentNode.insertBefore(fbds, s);
+			_fbq.loaded = true;
+			 }
+			 _fbq.push(['addPixelId', '1620419691530919']);
+		})();
+		window._fbq = window._fbq || [];
+		window._fbq.push(['track', 'PixelInitialized', {}]);
+	</script>
+	<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id=1620419691530919&amp;ev=PixelInitialized" /></noscript>
+	
+	
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<link rel="icon" type="image/png" href="https://www.eversafe.com/images/favicon.ico" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://www.eversafe.com/index.html" />
+	<meta property="og:site_name" content="EverSafe" />
+	
+	<!-- stylesheets -->
+	<link rel="stylesheet" type="text/css" href="https://www.eversafe.com/css/compiled/theme.css?v=1"/>
+	<link rel="stylesheet" type="text/css" href="https://www.eversafe.com/css/vendor/animate.css?v=1"/>
+	<link rel="stylesheet" type="text/css" href="https://www.eversafe.com/css/vendor/entypo.css?v=1">
+	<link rel="stylesheet" type="text/css" href="https://www.eversafe.com/css/vendor/font-awesome.min.css?v=1"/>
+	
+	<!-- javascript -->
+	<script src="https://www.eversafe.com/scripts/jquery/jquery-1.10.2.min.js?v=1"></script>
+	<script src="https://www.eversafe.com/js/bootstrap/bootstrap.min.js?v=1"></script>
+	<script src="https://www.eversafe.com/js/theme.js?v=1"></script>
+	
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	
+	<script src="https://www.eversafe.com/js/riveted.min.js"></script>
+	<script>riveted.init({reportInterval: 5});</script>
+	<script src="https://www.eversafe.com/js/jquery.scrolldepth.min.js"></script>
+	<script>$(function() { $.scrollDepth({
+				   elements : [], percentage: true, pixelDepth: false});
+		   });
+	</script>
+
+</head>
+<body id="features">
+	
+	<div class="hide"><script type="text/javascript">
+		var url = 'https://www.eversafe.com/seam/resource/timage?ts=' + new Date().getTime();
+		url += (window.location.search ? "&" + window.location.search.substring(1) : "");
+		url += '&path=' + unescape(encodeURIComponent(window.location.pathname));
+		document.write('<img width="1" height="1" src="' + url + '">');
+	</script></div>
+
+	<header class="navbar navbar-fixed-top navbar-inverse white" role="banner">
+		<div class="container">
+	
+			<div class="log col-md-12 hidden-xs" style="z-index: 10 !important;">
+				<a href="https://www.eversafe.com/static/login.scan"><span class="entypo-key"></span>Log In</a><a class="button button-signup" href="https://www.eversafe.com/pricing.html"
+					style="padding-top: 4px; padding-bottom: 4px; z-index: 10 !important;" >Free 30-Day Trial</a>
+			</div>
+	
+			<div class="navbar-header">
+				<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<div class="homelogo">
+					<a href="https://www.eversafe.com/index.html" class="navbar-brand"><img src="https://www.eversafe.com/images/logos/logo.png" alt="EverSafe"></a>
+					<a class="navbar-brand" href="https://www.eversafe.com/index.html"><span title="Home" class="entypo-house"></span></a> 
+					<a class="navbar-brand" href="https://www.eversafe.com/contact-us.html"><span title="Contact Us" class="entypo-mail"></span></a> 
+						
+					<a href="https://www.eversafe.com/index.html" class="navbar-small"><img src="https://www.eversafe.com/images/logos/logo-i.png" alt="EverSafe"></a> 
+					<a class="navbar-small" href="https://www.eversafe.com/index.html"><span class="entypo-house"></span></a>
+					<a class="navbar-small" href="https://www.eversafe.com/contact-us.html"><span class="entypo-mail"></span></a>
+				</div>
+			</div>
+		
+			<nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="dropdown "><a href="https://www.eversafe.com/how-it-works.html" class="dropdown-toggle"> How It Works </a></li>
+					<li class="dropdown "><a href="https://www.eversafe.com/pricing.html" class="dropdown-toggle"> Pricing </a></li>
+					<li class="dropdown "><a href="https://www.eversafe.com/trusted-advocates.html" class="dropdown-toggle"> Family/Advocates </a></li>
+					<li class="dropdown "><a href="#" class="dropdown-toggle" data-toggle="dropdown"> About EverSafe <i class="fa fa-chevron-down"></i></a>
+						<ul class="dropdown-menu">
+							<li><a href="https://www.eversafe.com/our-story.html">Our Story</a></li>
+							<li><a href="https://www.eversafe.com/our-team.html">Our Team</a></li>
+							<li><a href="https://www.eversafe.com/eversafe-advisory-board.html">Advisory Board</a></li>
+							<li><a href="https://www.eversafe.com/press.html">Press</a></li>
+							<li><a href="https://www.eversafe.com/eversafe-appearances.html">EverSafe Appearances</a></li>
+							<li><a href="https://www.eversafe.com/contact-us.html">Contact us</a></li>
+						</ul>
+					</li>
+					<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"> Resources <i class="fa fa-chevron-down"></i></a>
+						<ul class="dropdown-menu">
+							<li><a href="https://www.eversafe.com/elder-financial-abuse/overview.html">Elder Financial Abuse</a></li>
+							<li><a href="http://www.eversafe.com/blog/">EverSafe Blog</a></li>
+							<li><a href="https://www.eversafe.com/blog/elder-financial-abuse-in-the-news">General News</a>
+							<!--<li><a href="https://www.eversafe.com/press.html">Press</a></li> -->
+						</ul>
+					</li>
+					<li class="dropdown"><a class="visible-xs entypo-key" href="https://www.eversafe.com/static/login.scan">Log In</a></li>
+					<li class="dropdown"><a class="visible-xs" href="#" onclick="enrollment.startEnrollmentProcess(0); return false;">Sign Up</a></li>
+				</ul>
+			</nav>
+			
+			
+		</div>
+	</header>
+	
+	<!-- ###################################################################################### -->
+	<!-- ###################################################################################### -->
+	<!-- ###################################################################################### -->
+	
+	<div id="pageContent" style="min-height: 350px;">
+		PAGE CONTENT
+	</div>
+	
+	<!-- ###################################################################################### -->
+	<!-- ###################################################################################### -->
+	<!-- ###################################################################################### -->
+
+	
