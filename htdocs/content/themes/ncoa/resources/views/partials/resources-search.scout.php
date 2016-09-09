0add8a44d10b0e177b4d4de0169fad100b84d8a9
Stephen Engels <sgengels@gmail.com>
2016-09-08 20:20:55 -0500

    update to search page template

--- htdocs/content/themes/ncoa/resources/views/partials/resources-search.scout.php
+++ htdocs/content/themes/ncoa/resources/views/partials/resources-search.scout.php
@@ -12,27 +12,15 @@
 </script>
 
 <div ncoa-form>
-	<div class="form-group col-md-4">
-    	<label for="search-programs">Search Programs</label>
-	    <input type="text" class="form-control" ng-model="$root.prg_nm_filter" id="search-programs" placeholder="Enter Keyword">
-	</div>
-	<div class="form-group col-md-3 col-lg-3">
-  	<label for="search-programs">Search Categories</label>
-  	<select name="category" ng-model="category" class="form-control" required>
-        <option value="">-- Select --</option>
-	  		@foreach($categories as $key => $value)
-			    <option value="{{ $key }}">{{ $value }}</option>
-		    @endforeach
-		</select>
-	</div>
-	<div class="form-group col-md-3">
+	
+	<div class="form-group col-md-10">
   	<label for="search-states">Search States</label>
   	<select name="state" ng-model="state" class="form-control" required>
-      <option value="">-- Select --</option>
+        <option value="">-- Select --</option>
   		@foreach($states as $key => $value)
 		    <option value="{{ $key }}">{{ $value }}</option>
 	    @endforeach
-    </select>
+        </select>
 	</div>
 	<div class="form-group col-md-2">
 		<button class="btn btn-primary" ng-click="processForm(form, $event)">Find</button>
