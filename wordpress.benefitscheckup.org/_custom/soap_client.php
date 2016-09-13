39601c2ab156f888715289b589a7ab1aa7e9629e
Аюкасов Рустам <raukasov@gmail.com>
2016-09-13 17:27:37 +0300

    removed trash files

--- wordpress.benefitscheckup.org/_custom/soap_client.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-require_once('CachedSoapClient.php');
-
-$url = "https://".$_SERVER['SERVER_NAME']."/util/remotescreening_3_9_1.cfc?WSDL";
-//cache or no cache
-$soapClient = new CachedSoapClient($url, array('cache_wsdl' => WSDL_CACHE_NONE));
-//$soapClient = new CachedSoapClient($url, array('cache_wsdl' => WSDL_CACHE_BOTH), $subsetID, doScreening);
-
-?>
--- wordpress.benefitscheckup.org/_custom/util/affinity_client.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-
-$url = "https://www.benefitscheckup.org/util/affinity.cfc?WSDL";
-$affinityClient = new soapClient($url, array('cache_wsdl' => WSDL_CACHE_NONE));
-
-
-
-?>
