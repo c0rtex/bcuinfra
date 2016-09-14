70c6e60dc6e31463fb756f56307f3a1fbd8a2fd7
Аюкасов Рустам <raukasov@gmail.com>
2016-09-14 18:49:35 +0300

    Added help popup on prescreen questionnaire

--- htdocs/content/themes/ncoa/resources/assets/js/app.js
+++ htdocs/content/themes/ncoa/resources/assets/js/app.js
@@ -722,6 +722,20 @@ app.directive('questionDiv',['questionTemplates',function(questionTemplates) {
 	}
 }]);
 
+app.directive('script', function() {
+	return {
+		restrict: 'E',
+		scope: false,
+		link: function(scope, elem, attr) {
+			if (attr.type === 'text/javascript-lazy') {
+				var code = elem.text();
+				var f = new Function(code);
+				f();
+			}
+		}
+	};
+});
+
 app.directive('textSizeChanger', ['$document', 'localStorageService', function ($document, localStorageService) {
 	return {
 	  restrict: 'E',
