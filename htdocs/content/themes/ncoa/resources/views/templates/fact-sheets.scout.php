1d69c84b96deefcf5a7c4fb01e43a4206789fea2
Lynna Cekova <lynna.cekova@gmail.com>
2016-09-22 12:57:54 -0400

    more changes

--- htdocs/content/themes/ncoa/resources/views/templates/fact-sheets.scout.php
+++ htdocs/content/themes/ncoa/resources/views/templates/fact-sheets.scout.php
@@ -120,8 +120,35 @@ echo '
 ?>
 
             @if ($app_forms)
+            <?php $pos=1 ?>
             @foreach($app_forms as $ekey => $evalue)
+            
+            <?php $pos++ ?>
+
+            @if ($pos === 4)
+
+<!-- Trigger the modal with a button -->
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">See More</button>
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">More Applications</h4>
+      </div>
+      <div class="modal-body">
+
+
+
+      
+            @endif
+
             @if(strpos($evalue->url, 'http')===0)
+
             <a href="{{$evalue->url }}" target="_blank" class="btn btn-link fact-sheet-button-fwd fact-sheets-side-link">
             <span style="white-space: pre-line">{{ $evalue->caption }}</span>
             </a>
@@ -132,7 +159,21 @@ echo '
             </a>
             @endif
 
+            
+            
             @endforeach
+            @if ($pos >= 4)
+</div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+            @endif
             @endif
 
             
