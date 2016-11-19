5f4ae00ccc51e15c74fcd84b0ad7e48764f39f23
Rustam Ayukasov <raukasov@gmail.com>
2016-11-19 14:00:30 +0300

    BCURD-46 changed design for citizen question

--- htdocs/content/themes/ncoa/resources/assets/js/app.js
+++ htdocs/content/themes/ncoa/resources/assets/js/app.js
@@ -1005,6 +1005,7 @@ app.factory('questionTemplates',[function() {
         "marital_stat":directivePath+"marital_stat.html",
         "bcuqc_income":directivePath+"bcuqc_income.html",
         "bcuqc_interest_category":directivePath+"bcuqc_interest_category.html",
+        "citizen":directivePath+"citizen.html",
         "sp_veteran":directivePath+"sp_veteran.html",
         "incomegrid":directivePath+"incomegrid.html",
         "assetgrid":directivePath+"assetgrid.html"};
