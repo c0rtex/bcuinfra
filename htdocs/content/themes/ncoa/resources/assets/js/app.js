4874e8f6f8b39152777b3e9ef3aa5d60b85044f9
Juan Esteban Garcia <ctxuser@gmail.com>
2016-12-23 08:21:27 -0500

    BCURD-300: List of programs and categories to print

--- htdocs/content/themes/ncoa/resources/assets/js/app.js
+++ htdocs/content/themes/ncoa/resources/assets/js/app.js
@@ -2203,13 +2203,22 @@ app.controller('questionnairePrescreenResultsController', ['$scope', '$state', '
 
     var returned_programs = [];
     $scope.found_programs = [];
+    $scope.available_fact_sheets = {};
 
     prescreen.data.results.found_programs.forEach(function(element) {
         if (prescreen.data.answers[element.category]) {
             returned_programs.push(element.category);
             $scope.found_programs.push(element);
         }
+
+        $scope.available_fact_sheets[element.category] = [];
+        element.programs.forEach(function(program) {
+            $scope.available_fact_sheets[element.category].push(program.code);
+        });
     });
+
+    // BCURD-300: List of categories & programs.
+    console.log(encodeURIComponent(JSON.stringify($scope.available_fact_sheets)));
 
     // Find empty programs
     var programs_diff = $(prescreen.data.screenData.benefits_categories_codes).not(returned_programs).get();
