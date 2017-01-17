44dedb67359bc0997421b55772db9b608bcfcf84
Juan Esteban Garcia <ctxuser@gmail.com>
2017-01-17 13:57:39 -0500

    BCURD-258: Responsive issues on short fact sheet on Android phone

--- htdocs/content/themes/ncoa/resources/views/templates/short-fact-sheets.scout.php
+++ htdocs/content/themes/ncoa/resources/views/templates/short-fact-sheets.scout.php
@@ -20,7 +20,7 @@
         <div class="card-nested">
             <!-- Desktop -->
             <div class="row">
-                <div class="col-md-9 hidden-xs hidden-sm">
+                <div class="col-xs-12 col-md-9">
                     <!-- Views -->
                     <div>
                         <span class="icon-block pull-left">
@@ -75,7 +75,7 @@
                     @endif
                 </div>
 
-                <div class="col-md-3 hidden-xs hidden-sm">
+                <div class="col-xs-12 col-md-3">
                     <?php
                     $programUrl = trim(Meta::get(Loop::id(), $key = 'program-url', $single = true));
                     ?>
@@ -90,87 +90,10 @@
                     @if(!empty($programUrl))
                     <div class="results-options">
                         <span class="fact-sheets-side-header">Quick Links</span>
-
+                        <br />
                         <a target="_blank" href="{{ $programUrl }}" class="btn btn-link fact-sheet-button-fwd fact-sheets-side-link">Program Website</a>
                     </div>
                     @endif
-                </div>
-
-                <!-- Mobile -->
-                <div class="col-xs-12 hidden-md hidden-lg">
-                    <div>
-                        <span class="icon-block-healthcare pull-left"></span>
-                        <div class="pull-left">
-                            <div class="fact-sheets-header fact-sheets-top-header">{{ Meta::get(Loop::id(), $key = 'fact-sheet-category', $single = true) }}</div>
-                            <div class="fact-sheets-header fact-sheets-bottom-header">{{ Loop::title() }}</div>
-                        </div>
-                        <div class="clearfix"></div>
-                        <hr class="fact-sheets-hr" />
-                        <div class="fact-sheets-icon-block pull-left">
-                            <a href="{{ Loop::link() }}?print=y" class="fact-sheet-header-icon">
-                                <span class="fa fa-print"></span>
-                                Print
-                            </a>
-                            <span class="fact-sheets-spacer"></span>
-                            <a href="{{ Loop::link() }}?pdf=y" class="fact-sheet-header-icon">
-                                <span class="fa fa-floppy-o"></span>
-                                Save
-                            </a>
-                            <!--<span class="fact-sheets-spacer"></span>
-                            <a href="#" class="fact-sheet-header-icon">
-                                <span class="fa fa-envelope-o"></span>
-                                Email
-                            </a>-->
-                        </div>
-                    </div>
-                    <div class="clearfix"></div>
-                    <hr class="fact-sheets-hr" />
-                    <div class="mobile-padding">
-                        {{ Loop::content() }}
-
-                        <div class="results-options fact-sheet-options">
-                            <div class="fact-sheets-know-container">
-                                <span class="fact-sheets-know fact-sheets-side-header">{{ Meta::get(Loop::id(), $key = 'title', $single = true) }}</span>
-                                <br />
-                                {{ Meta::get(Loop::id(), $key = 'body-copy', $single = true) }}
-                            </div>
-                        </div>
-
-                        @if($is_alt)
-                        {{ Meta::get(Loop::id(), $key = 'requirement-info-alt', $single = true) }}
-                        @endif
-
-                        @if(!$is_alt)
-
-                        <div>
-                            <a href="{{ Meta::get(Loop::id(), $key = 'program-url', $single = true) }}" target="_new" class="btn btn-link fact-sheet-button-fwd fact-sheets-side-link">{{ Meta::get(Loop::id(), $key = 'program-title', $single = true) }}</a>
-                        </div>
-
-                        <hr class="fact-sheets-hr" />
-
-                        {{ Meta::get(Loop::id(), $key = 'requirement-info', $single = true) }}
-
-                        <div class="fact-sheets-main-apply mobile-padding">
-                            <a href="{{ Meta::get(Loop::id(), $key = 'button-url', $single = true) }}" class="btn btn-primary fact-sheets-main-mobile">{{ Meta::get(Loop::id(), $key = 'button-title', $single = true) }}</a>
-                        </div>
-
-                        <div class="mobile-padding">
-                            <a href="{{ Meta::get(Loop::id(), $key = 'english-url', $single = true) }}" class="btn btn-link fact-sheets-link">
-                                <span class="fa fa-file fact-sheets-icon"></span>
-                                {{ Meta::get(Loop::id(), $key = 'english-title', $single = true) }}
-                            </a>
-                            <span class="fact-sheets-bar">|</span>
-                            <a href="{{ Meta::get(Loop::id(), $key = 'spanish-url', $single = true) }}" class="btn btn-link fact-sheets-link">
-                                <span class="fa fa-file fact-sheets-icon"></span>
-                                {{ Meta::get(Loop::id(), $key = 'spanish-title', $single = true) }}
-                            </a>
-                            <span class="fact-sheets-bar">|</span>
-                            <a href="{{ Meta::get(Loop::id(), $key = 'more-languages-url', $single = true) }}" class="btn btn-link fact-sheet-button-fwd fact-sheets-link">
-                                {{ Meta::get(Loop::id(), $key = 'more-languages', $single = true) }}
-                            </a>
-                        </div>
-                        @endif
-                    </div>
                 </div>
             </div>
         </div>
