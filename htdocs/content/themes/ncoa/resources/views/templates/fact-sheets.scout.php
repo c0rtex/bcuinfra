92f84e83321870295e541429ed5299ad8139f493
S Engels <sgengels@gmail.com>
2017-02-02 16:15:33 -0600

    BCURD 318

--- htdocs/content/themes/ncoa/resources/views/templates/fact-sheets.scout.php
+++ htdocs/content/themes/ncoa/resources/views/templates/fact-sheets.scout.php
@@ -2,7 +2,6 @@
 <?php $snap_findstores_url = trim(Meta::get(Loop::id(), $key = 'find_stores_to_use_debit_card_in_english_and_spanish', $single = true)); ?>
 <?php $snap_bcu_url = trim(Meta::get(Loop::id(), $key = 'find_out_if_youre_eligible', $single = true)); ?>
 <?php $programUrl = trim(Meta::get(Loop::id(), $key = 'program-url', $single = true)); ?>
-<?php echo "test".$snap_findstores_url.$snap_bcu_url;?>
 
 @extends($layout)
 
