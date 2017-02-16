ac30aa0783dfc9b980df5b4fd5b4225033ff9e61
S Engels <sgengels@gmail.com>
2017-02-16 14:50:57 -0600

    bcurd318

--- htdocs/content/themes/ncoa/resources/views/partials/footer.scout.php
+++ htdocs/content/themes/ncoa/resources/views/partials/footer.scout.php
@@ -1,10 +1,10 @@
-@include('partials.footer-alternate')
+﻿@include('partials.footer-alternate')
 <div class="container footer-intro">
     <div class="row">
         <div class="col-sm-12">
             <div class="site-logo">
                 <a href="http://www.ncoa.org" title="NCOA" rel="home">
-                    <img width="177" height="60" src="/content/uploads/2016/09/ncoa-logo-177x60.png" class="header-logo" alt="National Council on Aging"  sizes="(max-width: 177px) 100vw, 177px">
+                    <img width="177" height="60" src="/content/uploads/2016/09/ncoa-logo-177x60.png" class="header-logo" alt="National Council on Aging" title="National Council on Aging"  sizes="(max-width: 177px) 100vw, 177px">
                 </a>
             </div>
 
