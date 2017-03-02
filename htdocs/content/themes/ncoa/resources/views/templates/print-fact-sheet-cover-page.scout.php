9f75cf980cc050809b703eb3f5d3b90afd6c05f9
Juan Esteban Garcia <ctxuser@gmail.com>
2017-03-02 13:40:07 -0500

    Restoring cover page for fact sheets (PDF)

---
+++ htdocs/content/themes/ncoa/resources/views/templates/print-fact-sheet-cover-page.scout.php
@@ -0,0 +1,20 @@
+<div style="page-break-before: auto;">
+@extends('layouts.print')
+@section('main')
+
+
+<center>
+    <hr>
+    <br><br><br><br><br><br>
+    <p style="text-align: center;">&nbsp;<img class="size-small  aligncenter" src="<?php echo themosis_assets() ?>/images/BCU-logo.jpg" alt="BenefitsCheckUp"></p>
+    <br><br><br><br><br><br>
+    <h2 style="text-align: center;">Your BenefitsCheckUp Report</h2>
+    <p style="text-align: center;"><strong>Brought to you by</strong></p>
+    <img class="size-full " src="<?php echo themosis_assets() ?>/images/ncoa-logo.png" alt="National Council on Aging" width="177" height="60">
+    <br><br><br><br><br><br>
+    <hr>
+
+    <h2 style="text-align: center;">Powered by BenefitsCheckUp.org</h2>
+</center>
+</div>
+@stop
