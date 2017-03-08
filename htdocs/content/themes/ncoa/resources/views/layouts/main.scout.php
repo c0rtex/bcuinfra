cf6b521934926982afbd0b536297917616b2321b
Juan Esteban Garcia <ctxuser@gmail.com>
2017-03-07 22:59:28 -0500

    BCURD-347

--- htdocs/content/themes/ncoa/resources/views/layouts/main.scout.php
+++ htdocs/content/themes/ncoa/resources/views/layouts/main.scout.php
@@ -6,32 +6,35 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="<?php echo themosis_assets() ?>/css/main.css">
     <script src="<?php echo themosis_assets() ?>/js/7683b69978.js"></script>
-    <?php wp_head(); ?>
-
+    <!-- Google Tag Manager -->
+    <script>
+      dataLayer = [
+        { 'partner_id': '0', 'partner_name': '', 'subset_id': '0', 'state': '', }
+      ];
+    </script>
     <script>(function (w, d, s, l, i) {
-      w[l] = w[l] || [];
-      w[l].push(
-        { 'gtm.start': new Date().getTime(), event: 'gtm.js' }
-      );
-      var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-      j.async = true;
-      j.src =
-        '//www.googletagmanager.com/gtm.js?id=' + i + dl;
-      f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-PHJ4LQ');</script>
+        w[l] = w[l] || [];
+        w[l].push(
+          { 'gtm.start': new Date().getTime(), event: 'gtm.js' }
+        );
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+          '//www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, 'script', 'dataLayer', 'GTM-PHJ4LQ');</script>
+    <!-- End Google Tag Manager -->
+
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+<!-- Google Tag Manager (noscript) -->
 <noscript>
   <iframe src="//www.googletagmanager.com/ns.html?id=GTM-PHJ4LQ"
           height="0" width="0" style="display:none;visibility:hidden"></iframe>
 </noscript>
-
-<script>
-    dataLayer = [
-        { 'partner_id': '0', 'partner_name': '', 'subset_id': '0', 'state': '', }
-    ];
-</script>
+<!-- End Google Tag Manager (noscript) -->
 
 @include('partials.header')
 
