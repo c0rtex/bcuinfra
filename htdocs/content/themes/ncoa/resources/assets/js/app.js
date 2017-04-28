19ca7b19839cd288f6c99f7a0ff63a855f7945bf
Rustam Ayukasov <raukasov@gmail.com>
2017-04-28 22:01:28 +0300

    BCURD-321. Restored get short program description from wp

--- htdocs/content/themes/ncoa/resources/assets/js/app.js
+++ htdocs/content/themes/ncoa/resources/assets/js/app.js
@@ -2483,8 +2483,6 @@ app.directive("divKeyProgram",['prescreen',function(prescreen) {
     }
 }]);
 
-/*
-TODO revert this change after resolve wp rest api plugin issue
 app.directive('divProgramDesc',['factSheet',function(factSheet) {
     return {
         restrict: 'E',
@@ -2498,20 +2496,7 @@ app.directive('divProgramDesc',['factSheet',function(factSheet) {
             program_code:"=programCode"
         }
     }
-}]);*/
-
-app.directive('divProgramDesc',function() {
-    return {
-        restrict: 'E',
-        //templateUrl:'/content/themes/ncoa/resources/views/pages/benefits-checkup/programs/programs.category.html?'+(new Date()),
-        link: function(scope, element) {
-            element.append("<p>"+scope.program_desc+"</p>");
-        },
-        scope: {
-            program_desc:"=programDesc"
-        }
-    }
-});
+}]);
 
 app.controller('questionnaireResultsController', ['$scope', '$state', 'screening', function($scope, $state, screening){
     var el = document.querySelector('.odometer');
