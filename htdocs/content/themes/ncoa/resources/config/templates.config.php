1d6dff11aabf45d93577ac65bf982e7f26de25de
Joshua Berry <josh.berry@ogilvy.com>
2017-06-23 16:44:32 -0400

    - reverting to prior revision, prepare for new merge

--- htdocs/content/themes/ncoa/resources/config/templates.config.php
+++ htdocs/content/themes/ncoa/resources/config/templates.config.php
@@ -13,10 +13,12 @@ return [
 	'benefitscheckup-template'          => 'Benefits Checkup Page',
 	'resources-template'				=> 'Resources Library Page',
 	'resources-results-template'		=> 'Resources Results Page',
+    'echo-template'	                	=> 'Echo Page',
 	'program-details-template'			=> 'Program Details Page',
 	'program-snap-template'				=> 'Program Snap Page',
 	'sponsors-template'					=> 'Sponsors Page',
 	'AboutUsTemplate' 					=> 'About Us Page',
+	'echotest-template' 					=> 'Echo Test Page',
 	'white-label-homepage-template' 	=> 'White Label Homepage',
 	'fact-sheets-template'				=> 'Fact Sheet Template'
 ];
