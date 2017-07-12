614e95d7830a49bf9dbd6f490c729c94ff069ae2
Zachary Meredith <zachary@echo.co>
2017-07-12 15:17:22 -0500

    livechat updates

--- htdocs/content/themes/ncoa/resources/assets/js/app.js
+++ htdocs/content/themes/ncoa/resources/assets/js/app.js
@@ -2744,3 +2744,14 @@ $(document).on('click', '.accordian-trigger', function(e) {
 });
 
 $('#menu-primarynav li.current-menu-item a').wrapInner('<span></span>');
+
+// Start of LiveChat (www.livechatinc.com) code
+<!-- Start of LiveChat (www.livechatinc.com) code -->
+window.__lc = window.__lc || {};
+window.__lc.license = 8840876;
+(function() {
+  var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
+  lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
+})();
+<!-- End of LiveChat code -->
