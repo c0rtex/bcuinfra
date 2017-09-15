2c9039e940c5c382478dddce27e4df760d9d3ac1
Joshua Berry <josh.berry@ogilvy.com>
2017-09-14 20:36:29 -0400

    hide disaster warning on disaster page

--- htdocs/content/themes/ncoa/resources/views/partials/header.scout.php
+++ htdocs/content/themes/ncoa/resources/views/partials/header.scout.php
@@ -31,10 +31,17 @@
             @endif
         </div>
 
-				<div class="flood-banner" style="color: black; font-size: 18px;">
-					<p><span style="color: red; font-weight:bold;">Hurricane Disaster Relief:</span> If you live in an area that was affected by Hurricane Harvey, Hurricane Irma, or any other disaster, you may be able to get help. <a href="/disaster-assistance" style="color: black;">Find help now!</a>
-					</p>
-				</div>
+				<?
+				$page_id = get_the_ID();
+				//13578 - disaster page id
+				?>
+				
+				<?php if ($page_id != 13578): ?>
+					<div class="flood-banner" style="color: black; font-size: 18px;">
+						<p><span style="color: red; font-weight:bold;">Hurricane Disaster Relief:</span> If you live in an area that was affected by Hurricane Harvey, Hurricane Irma, or any other disaster, you may be able to get help. <a href="/disaster-assistance" style="color: black;">Find help now!</a>
+						</p>
+					</div>
+				<?php endif; ?>
 
         <nav class="navbar navbar-default">
             <div>
