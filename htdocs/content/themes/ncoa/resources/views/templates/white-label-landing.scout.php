8cc020c5419db10e716708b8e8e4ee0ee166ba03
Juan Esteban Garcia <ctxuser@gmail.com>
2017-09-25 17:49:19 -0500

    PLR-23: Layout changes

--- htdocs/content/themes/ncoa/resources/views/templates/white-label-landing.scout.php
+++ htdocs/content/themes/ncoa/resources/views/templates/white-label-landing.scout.php
@@ -7,7 +7,7 @@
 </script>
 
 <div ng-controller="becsController" class="white-wrapper">
-    <div class="container resources container--small">
+    <div class="container resources text-center">
 
       <section class="white-label-landing">
         <header class="white-label-landing__header">
@@ -15,8 +15,8 @@
           <p class="subtitle">Select your state and Benefits Enrollment Center below to proceed.</p>
         </header>
   
-        <form class="white-label-landing__form">
-          <div class="form-item">
+        <form>
+          <div class="form-item form-item-white-label-landing-state">
             <label>Select Your State</label>
             <select class="form-control" ng-model="state" ng-options="becsState.value for becsState in becsStates track by becsState.id">
               <option selected="selected" value="">-- choose a state --</option>
@@ -25,7 +25,7 @@
 
           <div class="white-label-landing__form-then">then</div>
 
-          <div class="form-item">
+          <div class="form-item form-item-white-label-landing-org">
             <label>Select Your Organization</label>
             <select ng-disabled="!state" class="form-control" ng-model="bec" ng-options="bec.name for bec in becs">
               <option selected="selected" value="">-- choose an option --</option>
